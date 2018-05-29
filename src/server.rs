e14e35ec4b74b99acffb7655cf2a14401a13d48b
Jenna Magius <pyrosim@gmail.com>
2018-05-29 11:01:01 -0600

    Well, the server has basically no state, we can save ourselves some heartache trying to find a safe path to reexec by not reexecing.

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,6 @@
 use arg;
 use core::Oxy;
 use std::net::{TcpListener, TcpStream};
-#[cfg(unix)]
-use std::os::unix::io::IntoRawFd;
 
 pub fn run() {
 	#[cfg(unix)]
@@ -11,7 +9,7 @@ pub fn run() {
 		info!("Listening on {:?}", arg::bind_address());
 		loop {
 			if let Ok((stream, _)) = listener.accept() {
-				reexec_stream(stream);
+				fork_and_handle(stream);
 			} else {
 				warn!("Error receiving connection?");
 			}
@@ -21,33 +19,19 @@ pub fn run() {
 	unimplemented!();
 }
 
-fn reexec_stream(stream: TcpStream) {
+fn fork_and_handle(stream: TcpStream) {
 	#[cfg(unix)]
 	{
-		use nix::unistd::{close, execve, fork, ForkResult::*};
-		use std::ffi::CString;
-		let fd = stream.into_raw_fd();
-
-		// Build everything needed for the exec before forking, because memory
-		// allocation isn't async-signal-safe
-		let mut args: Vec<String> = Vec::new();
-		args.push("oxy".to_string());
-		args.push("reexec".to_string());
-		args.push(format!("--fd={}", fd));
-		let args: Vec<CString> = args.into_iter().map(|x| CString::new(x).unwrap()).collect();
-		let mut env: Vec<String> = Vec::new();
-		env.push(format!("OXY_SERVER_ARGS={}", "TODO"));
-		let env: Vec<CString> = env.into_iter().map(|x| CString::new(x).unwrap()).collect();
-		// SECURITY: The Rust std API docs say this is dangerous, citing https://securityvulns.com/Wdocument183.html
-		// Let's not merge it to master with this.
-		let path = ::std::env::current_exe().unwrap();
-		let path = CString::new(path.to_string_lossy().into_owned()).unwrap();
+		use nix::unistd::{fork, ForkResult::*};
 
 		match fork() {
-			Ok(Parent { .. }) => close(fd).unwrap(),
+			Ok(Parent { .. }) => ::std::mem::drop(stream),
 			Ok(Child) => {
-				execve(&path, &args, &env).unwrap();
-				unreachable!();
+				Oxy::run(stream);
+				#[allow(unreachable_code)]
+				{
+					unreachable!();
+				}
 			}
 			Err(_) => warn!("Fork error"),
 		}
