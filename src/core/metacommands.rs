71780b2085d85ce23cb8b6416c5168f534823d80
jennamagius <jenna.the.mage@gmail.com>
2018-06-01 16:33:10 -0600

    Use Clap for Metacommand Argument Values (#48)
    Use clap more for metacommands

--- src/core/metacommands.rs
+++ src/core/metacommands.rs
@@ -11,13 +11,22 @@ use tuntap::{TunTap, TunTapType};
 
 fn create_app() -> App<'static, 'static> {
 	let subcommands = vec![
-		SubCommand::with_name("L").about("Create a local portforward."),
-		SubCommand::with_name("R").about("Create a remote portforward."),
+		SubCommand::with_name("L")
+			.about("Create a local portforward.")
+			.arg(Arg::with_name("local spec").index(1))
+			.arg(Arg::with_name("remote spec").index(2)),
+		SubCommand::with_name("R")
+			.about("Create a remote portforward.")
+			.arg(Arg::with_name("remote spec").index(1))
+			.arg(Arg::with_name("local spec").index(2)),
 		SubCommand::with_name("download")
 			.about("Download a file")
 			.arg(Arg::with_name("remote path").help("Remote file path to download from.").index(1))
 			.arg(Arg::with_name("local path").help("Local file path to download to.").index(2)),
-		SubCommand::with_name("upload").about("Upload a file"),
+		SubCommand::with_name("upload")
+			.about("Upload a file")
+			.arg(Arg::with_name("local path").index(1))
+			.arg(Arg::with_name("remote path").index(2)),
 		SubCommand::with_name("tun")
 			.about("Bridge two tun devices.")
 			.long_about(
@@ -35,10 +44,12 @@ fn create_app() -> App<'static, 'static> {
 		SubCommand::with_name("socks")
 			.about("Bind a local port as a SOCKS5 proxy server")
 			.arg(Arg::with_name("bind spec").index(1)),
-		SubCommand::with_name("pty").about(
-			"Open a remote PTY. \
-			 Happens by default, usually not necessary",
-		),
+		SubCommand::with_name("pty")
+			.about(
+				"Open a remote PTY. \
+				 Happens by default, usually not necessary",
+			)
+			.arg(Arg::with_name("command").index(1).default_value("bash")),
 		SubCommand::with_name("sh")
 			.about(
 				"Run a remote basic-command. \
@@ -49,7 +60,8 @@ fn create_app() -> App<'static, 'static> {
 				 Useful for Windows servers. \
 				 The command runs asyncronously and you don't get any output, \
 				 but you can pipe output to a file and then download it later.",
-			),
+			)
+			.arg(Arg::with_name("command").index(1).required(true)),
 		SubCommand::with_name("exit").about("Exits the Oxy client."),
 	];
 	let subcommands: Vec<App<'static, 'static>> = subcommands
@@ -67,35 +79,54 @@ fn create_app() -> App<'static, 'static> {
 }
 
 impl Oxy {
-	pub(super) fn handle_metacommand(&self, mut parts: Vec<String>) {
+	pub(super) fn handle_metacommand(&self, parts: Vec<String>) {
 		let matches = create_app().get_matches_from_safe(parts.clone());
 		match matches {
 			Err(clap::Error { message, .. }) => {
 				println!("{}", message);
 			}
-			Ok(matches) => {
-				match matches.subcommand_name().unwrap() {
+			Ok(matches2) => {
+				let name = matches2.subcommand_name().unwrap();
+				let matches = matches2.subcommand_matches(name).unwrap();
+				match name {
 					"sh" => {
-						self.send(BasicCommand { command: parts.remove(1) }); // TODO: ERROR_HANDLING
+						self.send(BasicCommand {
+							command: matches.value_of("command").unwrap().to_string(),
+						});
 					}
 					"pty" => {
-						let command = if parts.len() > 1 { parts.remove(1) } else { "bash".to_string() };
+						let command = matches.value_of("command").unwrap().to_string();
 						self.send(PtyRequest { command });
 					}
 					"download" => {
-						debug!("File transfer started");
-						let id = self.send(DownloadRequest { path: parts.remove(1) }); // TODO: ERROR_HANDLING
-						let file = File::create(parts.remove(1)).unwrap();
+						let file = File::create(matches.value_of("local path").unwrap().to_string());
+						if file.is_err() {
+							error!("Failed to open local file for writing: {}", matches.value_of("local path").unwrap());
+							return;
+						}
+						let file = file.unwrap();
+						let id = self.send(DownloadRequest {
+							path: matches.value_of("remote path").unwrap().to_string(),
+						});
+						debug!("Download started");
 						self.transfers_in.borrow_mut().insert(id, file);
 					}
 					"upload" => {
-						let id = self.send(UploadRequest { path: parts.remove(2) });
-						let file = File::open(parts.remove(1)).unwrap();
+						let file = File::open(matches.value_of("local path").unwrap());
+						if file.is_err() {
+							error!("Failed to open local file for reading: {}", matches.value_of("local path").unwrap());
+							return;
+						}
+						let file = file.unwrap();
+						let id = self.send(UploadRequest {
+							path: matches.value_of("remote path").unwrap().to_string(),
+						});
+						debug!("Upload started");
 						self.transfers_out.borrow_mut().push((id, file));
 					}
 					"L" => {
-						let remote_spec = parts.remove(2);
-						let local_spec = parts.remove(1);
+						let remote_spec = matches.value_of("remote spec").unwrap().to_string();
+						let local_spec = matches.value_of("local spec").unwrap().to_string();
 						let bind = TcpListener::bind(&local_spec.parse().unwrap()).unwrap();
 						let proxy = BindNotificationProxy {
 							oxy:   self.clone(),
@@ -116,29 +147,33 @@ impl Oxy {
 						self.port_binds.borrow_mut().insert(token_sized, bind);
 					}
 					"R" => {
-						let bind_id = self.send(RemoteBind { addr: parts.remove(1) });
-						self.remote_bind_destinations.borrow_mut().insert(bind_id, parts.remove(1));
+						let bind_id = self.send(RemoteBind {
+							addr: matches.value_of("remote spec").unwrap().to_string(),
+						});
+						self.remote_bind_destinations
+							.borrow_mut()
+							.insert(bind_id, matches.value_of("local spec").unwrap().to_string());
 					}
 					#[cfg(unix)]
 					"tun" => {
 						let reference_number = self.send(TunnelRequest {
 							tap:  false,
-							name: parts.remove(2),
+							name: matches.value_of("remote tun").unwrap().to_string(),
 						});
-						let tuntap = TunTap::create(TunTapType::Tun, &parts[1], reference_number, self.clone());
+						let tuntap = TunTap::create(TunTapType::Tun, matches.value_of("local tun").unwrap(), reference_number, self.clone());
 						self.tuntaps.borrow_mut().insert(reference_number, tuntap);
 					}
 					#[cfg(unix)]
 					"tap" => {
 						let reference_number = self.send(TunnelRequest {
 							tap:  true,
-							name: parts.remove(2),
+							name: matches.value_of("remote tap").unwrap().to_string(),
 						});
-						let tuntap = TunTap::create(TunTapType::Tap, &parts[1], reference_number, self.clone());
+						let tuntap = TunTap::create(TunTapType::Tap, matches.value_of("local tap").unwrap(), reference_number, self.clone());
 						self.tuntaps.borrow_mut().insert(reference_number, tuntap);
 					}
 					"socks" => {
-						let local_spec = parts.remove(1);
+						let local_spec = matches.value_of("bind spec").unwrap();
 						let bind = TcpListener::bind(&local_spec.parse().unwrap()).unwrap();
 						let proxy = SocksBindNotificationProxy {
 							oxy:   self.clone(),
